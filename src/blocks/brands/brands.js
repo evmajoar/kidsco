3f3febd279a31b11fb1ae55859eafaae97f03bd7
Arthur Dovhar <evmajoar@gmail.com>
2020-02-06 15:01:14 +0200

    Init commit

---
+++ src/blocks/brands/brands.js
@@ -0,0 +1,20 @@
+var mySwiper = new Swiper ('.brands__carousel', {
+  slidesPerView: 5,
+  loop: true,
+
+  pagination: {
+    el: '.brands__pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.brands__arrow--next',
+    prevEl: '.brands__arrow--prev',
+  },
+
+  breakpoints: {
+    1296: {
+      slidesPerView: 1
+    },
+  }
+});
